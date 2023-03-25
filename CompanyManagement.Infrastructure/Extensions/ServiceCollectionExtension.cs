22ed545c14d4b90738a6b77b3737d62a06d2adab
Michal Konwiak <michalkonwiak1@gmail.com>
2023-03-25 23:38:09 +0100

    insfrastructure design, create department entity

---
+++ CompanyManagement.Infrastructure/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,21 @@
+﻿using CompanyManagement.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyManagement.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<CompanyManagementDbContext>(options => options.UseSqlServer(
+                configuration.GetConnectionString("CompanyManagement")));
+        }
+    }
+}
---
+++ CompanyManagement.Infrastructure/Persistence/CompanyManagementDbContext.cs
@@ -0,0 +1,21 @@
+﻿using CompanyManagement.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompanyManagement.Infrastructure.Persistence
+{
+    public class CompanyManagementDbContext : DbContext
+    {
+
+        public CompanyManagementDbContext(DbContextOptions<CompanyManagementDbContext> options) : base(options)
+        {
+            
+        }     
+
+        public DbSet<Department> Departments { get; set; } 
+    }
+}
